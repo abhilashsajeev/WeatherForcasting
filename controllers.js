0446a25ab84ff3a4b0050f11d1198a73bdf4fddd
abhilash <abhilash@qburst.com>
2015-09-23 14:53:50 +0530

    Changed to controller as scheme

--- controllers.js
+++ controllers.js
@@ -1,22 +1,20 @@
 weatherApp.controller('homeController', ['$scope', 'cityService','$location', function($scope,cityService,$location){
-	$scope.city = cityService.city;
-	$scope.submit  = function(){
+	var vm = this;
+	vm.city = cityService.city;
+	vm.submit  = function(){
 		$location.path('/forcast');
 	};
-	$scope.$watch('city',function(){
-		cityService.city = $scope.city;
+	$scope.$watch('vm.city',function(){
+		cityService.city = vm.city;
 	});
 }]);
-weatherApp.controller('forcastController', ['$scope','cityService','$resource',
-	'$routeParams', function($scope,cityService,$resource,$routeParams){
-	$scope.city = cityService.city;
-	$scope.days = $routeParams.days || 2;
-	
-	$scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily',
-	 {callback:'JSON_CALLBACK'}, {get:{method:'JSONP'}});
-	$scope.weatherResult = $scope.weatherAPI.get({q:$scope.city,cnt:$scope.days,units:'metric'});
-	
-	$scope.convertToDate = function(dt){
+weatherApp.controller('forcastController', ['$scope','cityService',
+	'$routeParams', 'weatherService', function($scope,cityService,$routeParams,weatherService){
+	var vm = this;
+	vm.city = cityService.city;
+	vm.days = $routeParams.days || 2;
+	vm.weatherResult = weatherService.getWeather(vm.city,vm.days);
+	vm.convertToDate = function(dt){
 		return new Date(dt * 1000);
 	}
 }]);
--- routes.js
+++ routes.js
@@ -2,14 +2,14 @@ weatherApp.config(['$routeProvider',function($routeProvider) {
 	$routeProvider
 	.when('/',{
 		templateUrl : 'pages/home.htm',
-		controller:'homeController'
+		controller:'homeController as home'
 	})
 	.when('/forcast',{
 		templateUrl:'pages/forcast.htm',
-		controller:'forcastController'
+		controller:'forcastController as forcast'
 	})
 	.when('/forcast/:days',{
 		templateUrl:'pages/forcast.htm',
-		controller:'forcastController'
+		controller:'forcastController as forcast'
 	});
 }]);
