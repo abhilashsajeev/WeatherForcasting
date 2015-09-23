2d5b6ea6c4710bc27c1264b1f84a803ac75f0625
abhilash <abhilash@qburst.com>
2015-09-23 10:20:56 +0530

    Added service

--- app.js
+++ app.js
@@ -14,12 +14,33 @@ weatherApp.config(['$routeProvider',function($routeProvider) {
 	.when('/forcast',{
 		templateUrl:'pages/forcast.htm',
 		controller:'forcastController'
+	})
+	.when('/forcast/:days',{
+		templateUrl:'pages/forcast.htm',
+		controller:'forcastController'
 	});
-}])
-
-weatherApp.controller('homeController', ['$scope', function($scope){
-	
 }]);
-weatherApp.controller('forcastController', ['$scope', function($scope){
+
+weatherApp.service('cityService', function(){
+	this.city = 'NewYork, NY';
+});
+
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope,cityService){
+	$scope.city = cityService.city;
+	$scope.$watch('city',function(){
+		cityService.city = $scope.city;
+	})
+}]);
+weatherApp.controller('forcastController', ['$scope','cityService','$resource',
+	'$routeParams', function($scope,cityService,$resource,$routeParams){
+	$scope.city = cityService.city;
+	$scope.days = $routeParams.days || 2;
 	
-}])
+	$scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily',
+	 {callback:'JSON_CALLBACK'}, {get:{method:'JSONP'}});
+	$scope.weatherResult = $scope.weatherAPI.get({q:$scope.city,cnt:$scope.days,units:'metric'});
+	
+	$scope.convertToDate = function(dt){
+		return new Date(dt * 1000);
+	}
+}]);
