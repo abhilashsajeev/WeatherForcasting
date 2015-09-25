7f16a659008bcb59ea504e94767f44eba9f489c2
abhilash <abhilash@qburst.com>
2015-09-25 15:55:09 +0530

    Added Karma tests

---
+++ test/controllers.test.js
@@ -0,0 +1,45 @@
+"use strict";
+
+describe('HomeController',function () {
+	var homeController,cityServiceMock,scope,locationMock;
+	beforeEach(module('weatherApp'));
+	beforeEach(module(initMocks));
+  	beforeEach(inject(initController));
+  	
+  	it('Should exist',function(){
+  		expect(homeController).to.exist;
+  	});
+  	it('Default city should be set',function(){
+  		expect(homeController.city).to.equal('NewYork, NY');
+  	});
+
+  	it('Value of city in cityService should change if homeController.city changes',function(){
+  		homeController.city = 'London';
+  		scope.$digest(); // In order to invoke the $scope.$watch cycle
+  		expect(cityServiceMock.city).to.equal('London');
+  	});
+
+  	it('Should chage location on submit',function(){
+  		homeController.submit();
+  		expect(locationMock.path.calledWith('/forcast')).to.be.true
+  	})
+
+  	function initMocks($provide) {
+	    cityServiceMock = {
+	      city: 'NewYork, NY'
+	    };
+	    $provide.factory('cityService', function () {
+	      return cityServiceMock;
+	    });
+
+	}
+	function initController($controller,$rootScope){
+		scope = $rootScope.$new();
+		locationMock = sinon.stub({path:function(){}});
+		homeController = $controller('homeController', {
+	      $scope:scope,
+	      $location:locationMock
+	    });
+	}
+});
+
