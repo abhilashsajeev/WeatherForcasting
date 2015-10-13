66754a44c79989f19ee55d321dacfb3f83488478
Abhilash <abhialsh@qburst.com>
2015-10-13 10:13:12 +0530

    Added appid for the a api

--- src/services.js
+++ src/services.js
@@ -7,6 +7,6 @@ weatherApp.service('weatherService', ['$resource', function($resource){
   this.getWeather = function(city,days){
     var weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily',
       {callback:'JSON_CALLBACK'}, {get:{method:'JSONP'}});
-    return weatherAPI.get({q:city,cnt:days,units:'metric'});  
+    return weatherAPI.get({q:city,cnt:days,units:'metric',appid:'bd82977b86bf27fb59a04b61b657fb6f'});  
   }
 }]);
